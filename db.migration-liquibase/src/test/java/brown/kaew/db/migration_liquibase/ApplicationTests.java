0cff3bc840fb1a6ab415abf09b254a7ecb452289
brown-kaew <kaewtibkham@gmail.com>
2025-09-30 02:50:11 +0700

    init liquibase project

---
+++ db.migration-liquibase/src/test/java/brown/kaew/db/migration_liquibase/ApplicationTests.java
@@ -0,0 +1,13 @@
+package brown.kaew.db.migration_liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
