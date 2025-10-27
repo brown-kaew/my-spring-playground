df1b42518030d8e01ee8d7c5e6d19bcf9eac2fd2
brown-kaew <kaewtibkham@gmail.com>
2025-10-27 22:38:03 +0700

    Fix unittest

--- db.migration-flyway/src/test/java/brown/kaew/db/migration_flyway/ApplicationTests.java
+++ db.migration-flyway/src/test/java/brown/kaew/db/migration_flyway/ApplicationTests.java
@@ -1,13 +1,18 @@
 package brown.kaew.db.migration_flyway;
 
+import brown.kaew.db.migration_flyway.student.StudentRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockitoBean
+    StudentRepository studentRepository;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
--- db.migration-liquibase/src/test/java/brown/kaew/db/migration_liquibase/ApplicationTests.java
+++ db.migration-liquibase/src/test/java/brown/kaew/db/migration_liquibase/ApplicationTests.java
@@ -1,10 +1,15 @@
 package brown.kaew.db.migration_liquibase;
 
+import brown.kaew.db.migration_liquibase.student.StudentRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ApplicationTests {
+
+    @MockitoBean
+    StudentRepository studentRepository;
 
 	@Test
 	void contextLoads() {
