02f4d497459d4c414d8cda8082f1b8b28044760f
brown-kaew <kaewtibkham@gmail.com>
2025-10-27 22:38:27 +0700

    init kafka module

---
+++ kafka-experiment/src/main/java/brown/kaew/kafka_experiment/KafkaExperimentApplication.java
@@ -0,0 +1,13 @@
+package brown.kaew.kafka_experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExperimentApplication.class, args);
+	}
+
+}
---
+++ kafka-experiment/src/test/java/brown/kaew/kafka_experiment/KafkaExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package brown.kaew.kafka_experiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
