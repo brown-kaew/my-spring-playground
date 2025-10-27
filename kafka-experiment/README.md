77491754efde21e29a5ffc7571f44832aa562c41
brown-kaew <kaewtibkham@gmail.com>
2025-10-27 22:52:17 +0700

    init kafka producer

---
+++ kafka-experiment/README.md
@@ -0,0 +1,9 @@
+# Kafka experiment
+
+## Starting a Kafka broker with Docker
+
+To start a Kafka broker using Docker, you can use the following command:
+
+```bash
+docker compose -f kafka-experiment/compose.yaml up -d
+```
---
+++ kafka-experiment/src/main/java/brown/kaew/kafka_experiment/MessageController.java
@@ -0,0 +1,25 @@
+package brown.kaew.kafka_experiment;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public MessageController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public String publishMessage(@RequestBody String message) {
+        kafkaTemplate.send("test-topic", message);
+        return "Message published: " + message;
+    }
+}
+
